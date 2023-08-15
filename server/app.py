048a36d602a1fbb548766a8f68c52730f7140dea
Mark <ajineer@outlook.com>
2023-08-15 11:08:06 -0600

    finished lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,77 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        all_messages = Message.query.all()
+        messages_json = []
+
+        for message in all_messages:
+            message_dict = message.to_dict()
+            messages_json.append(message_dict)
+
+        response = make_response(
+            messages_json,
+            200
+        )
+
+        return response
+    
+    elif request.method == 'POST':
+        
+        data = request.get_json()
+        new_message = Message(
+            body = data["body"],
+            username = data["username"]
+            )
+        
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_dict = new_message.to_dict()
+        response = make_response(
+            jsonify(new_message_dict),
+            201
+        )
+        
+        return response
+
+@app.route('/messages/<int:id>', methods = ['PATCH', 'DElETE'])
 def messages_by_id(id):
-    return ''
+
+    message = Message.query.filter(Message.id == id).first()
+    
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+        db.session.add(message)
+        db.session.commit()
+        response = make_response(
+            jsonify(message.to_dict()),
+            200
+        )
+
+    elif request.method == 'DELETE':
+        
+        db.session.delete(message)
+        db.session.commit()
+
+        response = make_response(
+            {
+                "delete successful": True 
+            },
+            200
+        )
+
+        return response
+
+
+
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
